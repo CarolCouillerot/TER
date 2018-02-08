b0ce3d8b7e5b8c5be8eb8e7055c2009fef320501
dreyn <robz52@free.fr>
2018-02-08 17:59:44 +0100

    Légère modif.

--- pdfs/links_CL.sh
+++ pdfs/links_CL.sh
@@ -7,22 +7,18 @@ rm base
 
 while read line; do
     curl "https://aclweb.org/anthology/"$line > fic #télécharge la page contenant les pdf
-    grep -e "href=.\{,10\}\.pdf" ./fic -o > hrefs_ends #récupère les suffixes des liens des pdfs
-    cut -d '=' -f "2" hrefs_ends > clean1_ends # nettoyage des suffixes
-    cut -d '"' -f "1,2" clean1_ends --output-delimiter='' > clean2_ends
+    grep -e "J.\{7\}.pdf" ./fic -o > ends #récupère les suffixes des liens des pdfs
     while read end; do #va créer un fichier de liens dans de nombreux sous-dossiers
 		mkdir -p $line
 		cd $line
 		echo "https://aclweb.org/anthology/"$line$end >> links #envoie les liens dans links
 		cd "../.."
-    done < clean2_ends
+    done < ends
 done < result #la boucle s'exécute pour chaque ligne de ce fichier
 
 #supprime les fichiers inutiles
 rm fic
-rm hrefs_ends
-rm clean1_ends
-rm clean2_ends
+rm ends
 rm result
 
 mv J/ CL/
