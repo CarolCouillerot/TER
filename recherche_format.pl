5c67891d966fb6cd4732309aecfb774d53d00a11
Dreyn <robz52@free.fr>
2018-02-16 11:18:45 +0100

    Ajout de recherche sur les xml.

--- recherche_format.pl
+++ recherche_format.pl
@@ -10,41 +10,34 @@ my $matchIntro;
 my $matchRef;
 my $noMatchIntro;
 my $noMatchRef;
-my $matchSpec;
 
 my $nbFile = 0;
+my $nbXml = 0;
 my $nbIntro = 0;
+my $nbIntroXml = 0;
 my $nbRef = 0;
-my $nbSpec = 0;
 
 
 open ($noMatchIntro, ">noMatchIntro.txt");
 open ($noMatchRef, ">noMatchRef.txt");
 open ($matchSpec, ">matchSpec.txt");
 
-opendir(DIR, $dirName) or die "Could not open $dir\n";
+opendir(DIR, $dirName) or die "Could not open !! $dir\n";
 
 while (my $subDirName = readdir(DIR)) {
   if ($subDirName =~m /P[0|1][0-9]/){
-	  opendir($subDir, "$dirName$subDirName") or die "Could not open $subDir\n";
+	  opendir($subDir, "$dirName$subDirName") or die "Could not open ! $subDir\n";
 	  
 	  while (my $fileName = readdir($subDir)) {
 		  if ($fileName =~m /(.*).txt$/) {
 			  
-			  open($file, "<$dirName$subDirName/$fileName") or die "Could not open $file\n";
+			  open($file, "<$dirName$subDirName/$fileName") or die "Could not open... $fileName\n";
 			  
 			  $nbFile++;
-			  
-			  #print $matchSpec "\n\n$fileName :\n";
 			  
 			  $matchIntro = 0;
 			  $matchRef = 0;
 			  while (my $line = <$file>) {
-				  
-				  #if($line =~m /^.*/){
-					#  $nbSpec++;
-					#  print $matchSpec "$line\n"
-				  #}
 				  
 				  if($line =~m /^?([0-9]?[0-9]|[0-9]?[0-9] |[0-9]?[0-9]. )?(R ?E ?F ?E ?R ?E ?N ?C ?E ?S?|B ?i ?b ?l ?i ?o ?g ?r ?a ?p ?h ?y|(o ?n ?l ?i ?n ?e )?r ?e ?s ?o ?u ?r ?c ?e ?s?)(\.| ?:| ?-| ?\/)?\.?\n$/i){
 					  ++$matchRef;
@@ -53,12 +46,6 @@ while (my $subDirName = readdir(DIR)) {
 				  
 				  if($line =~m /^?([0-9]|[0-9] |[0-9]. )?(Introduction|I ?N ?T ?R ?O ?D ?U ?C ?T ?I ?O ?N)\.?\n$/){
 					  ++$matchIntro;
-				  }
-				  elsif($line =~m /^?([0-9]|[0-9] |[0-9]. )?(Introduction|INTRODUCTION|I N T R O D U C T I O N)\./)
-				  {
-					  ++$matchIntro;
-					  
-					  #print "$fileName : $line";
 				  }
 			  }
 			  if($matchIntro){
@@ -74,6 +61,31 @@ while (my $subDirName = readdir(DIR)) {
 			  
 			  close($file);
 		  }
+		  elsif ($fileName =~m /(.*).xml$/) {
+			  open($file, "<$dirName$subDirName/$fileName") or die "Could not open : $fileName\n";
+			  
+			  $nbXml++;
+			  
+			  $matchIntro = 0;
+			  $matchRef = 0;
+			  while (my $line = <$file>) {
+				  
+				  if(!$matchIntro and $line =~m />?([0-9]|[0-9] |[0-9]. )?(Introduction|I ?N ?T ?R ?O ?D ?U ?C ?T ?I ?O ?N)\.?<\//){
+					  ++$matchIntro;
+					  #print "$fileName : $line";
+				  #}elsif(){
+					#  ++$matchIntro;
+					#  print "$fileName : $line";
+				  }
+			  }
+			  if($matchIntro){
+				  $nbIntroXml++;
+			  }else{
+				  print $noMatchIntro "Xml : $fileName\n"
+			  }
+			  
+			  close($file);
+		  }
 	  }
 	  
 	  closedir($subDir);
@@ -81,9 +93,10 @@ while (my $subDirName = readdir(DIR)) {
 }
 
 print "Nombre de fichiers : $nbFile.\n";
+print "Nombre de fichiers xml : $nbXml.\n";
 print "Nombre d'Introduction : $nbIntro.\n";
+print "Nombre d'Introduction Xml : $nbIntroXml.\n";
 print "Nombre de réferences : $nbRef.\n";
-#print "Nombre de caractères spéciaux : $nbSpec.\n";
 
 close($noMatchIntro);
 close($noMatchRef);
