083e8b9cd8918db5cc0872d54941b0c2babc73a4
Carol Couillerot <carol.couillerot@gmail.com>
2018-03-21 13:15:21 +0100

    ajout analyse diachronique

--- depouille_v6.pl
+++ depouille_v6.pl
@@ -2,6 +2,10 @@
 # ROLE : lit les textes txt et cherche les langues
  # modif apres proposition JE
 #----------------------------------------
+# Active les avertissements de l'interpréteur
+use warnings;
+use POSIX;
+
 # Ces variables globales sont utilisées dans parcours et traite_texte,
 #  elles sont aussi redéclarées dans lecture
   my $nb_langues = 0;
@@ -25,8 +29,9 @@ my %res_langues;
 
 #concordance("corpus_ACL.txt");
 #traitement_paires("res_paires_complement.txt", "res_paires_complement2.txt");
-pretraitements("textes_adresses.txt", "resultats_lecture.csv", "erreurs_lecture.txt", "corpus_lecture");
+# pretraitements("textes_adresses.txt", "resultats_lecture.csv", "erreurs_lecture.txt", "corpus_lecture");
 #etude_comparative("textes_adresses.txt", "erreurs_lecture.txt");
+analyse_diachronique("textes_adresses.txt", 5, "counter_diachronique.csv");
 
 #----------------------------------------
 # IN : fichier TRACE nettoyé
@@ -547,4 +552,181 @@ sub lecture_comparative {
 	}
 
 	close($counter);
+}
+
+#----------------------------------------
+# Effectue une analyse temporelle : les occurences des langues par période
+# Entrée : corpus, langue.csv, pays.csv, fausses_langues.csv, taille de la période
+# Sortie : 1 fichiers "analyse_dia.txt" avec le nombre d'occurences de langues par période
+sub analyse_diachronique {
+	my $donnees = $_[0];
+	my $periode = $_[1];
+	my $fichier_res = $_[2]; 
+	my $line;
+	my $fic;
+	my $i;
+	my $langue;
+	my $pays;
+	my $nb_langues = 0;
+	my $nb_fausses_langues = 0;
+	my $nb_pays = 0;
+	my $nb_textes = 0;
+	my $nb_files = `wc -l $donnees | grep -o "[0-9]*"`;
+	my $texte;
+	my $ind_periode = 0;
+	my $date_prec = "00";
+	my $num_periode = 1;
+	my $numberOfYear = 18;
+	my $numberOfPeriod = ceil($numberOfYear/$periode);
+	my @nbTextesParPeriode;
+
+	my @langScore; #compte le nombre d'occurence par langue
+
+
+	for $i (1 .. $numberOfPeriod) { $nbTextesParPeriode[$i] = 0; }
+
+	open(TRACE, ">trace_diachronique.txt") || die "Je ne peux ouvrir le fichier trace_diachronique.txt $!";
+	open(RES, ">$fichier_res") || die "Je ne peux ouvrir le fichier $fichier_res $!";
+
+	#............................................................liste de langues
+	open(IN, "<langues4.csv ")|| die "Je ne peux ouvrir le fichier $fic $!";
+	while ($line = <IN>) {
+		if($line =~m /[a-z]/){ #élimine les lignes vides
+			chomp($line);
+			
+			$langue = "";
+			if($line =~m /^(.*)(\r|\n)$/){ # Pour se débarasser du retour à la ligne...
+				$langue = $1;
+			}
+			$tab_langues[$nb_langues] = $langue;
+			$langScore[$nb_langues] = [ initTab($numberOfPeriod,$langue) ];
+			$nb_langues++;
+		}
+	}
+	close(IN);
+	#............................................................liste de fausses langues
+	open(IN, "<fausses_langues.txt ")|| die "Je ne peux ouvrir le fichier $fic $!";
+	while ($line = <IN>) {
+		chop($line);
+		$langue = $line;
+		$tab_fausses_langues[$nb_fausses_langues] = $langue;
+		$nb_fausses_langues++;
+	}
+	close(IN);
+	#............................................................liste de pays
+	open(IN, "<pays.csv ")|| die "Je ne peux ouvrir le fichier $fic $!";
+	while ($line = <IN>) {
+		chomp($line);
+		if($line =~m /([a-zA-Z -]+)/){ # Juste pour se débarasser une bonne fois pour toute du retour à la ligne
+			$pays = $1;
+		}
+		
+		$tab_pays[$nb_pays] = $pays;
+		$nb_pays++;
+	}
+	close(IN);
+
+	open(IN, "<$donnees ")|| die "Je ne peux ouvrir le fichier $fic $!";
+
+	while ($line = <IN>) {
+		chop($line);
+		$fic = $line;
+		my $date;
+		if ($fic =~m /P([0-9][0-9])/) {
+			$date = $1; 
+		}
+		if( $date > $date_prec) { 
+			$ind_periode++;
+			$date_prec = $date;
+
+			if($ind_periode >= $periode) {
+				$ind_periode = 0;
+				$num_periode++;
+			}
+		}
+		# nombre de fichiers traités / nombre total de fichier * 100, permet d'afficher une progression
+		my $tmp = sprintf "%.2f", scalar( ($nb_textes/$nb_files)*100); 
+		print STDOUT "\rProgression : $tmp%"; # l'option \r permet à la progression de rester sur la même ligne du terminal. Pour cela, aucun autre print STDOUT doit être fait dans la boucle.
+
+		#............................................................traitement d'un texte
+		open(TEXT, "<$fic ")|| die "Je ne peux ouvrir le fichier $fic $!";
+		$nb_textes++;
+		$nbTextesParPeriode[$num_periode]++;
+		$texte = "";
+
+		while ($line = <TEXT>) {
+			chomp $line;
+			$texte .= " ".$line;
+		}
+		close(TEXT);
+		
+		#............................................................recherche des langues
+		#................ destruction d'expressions generatrices de bruit (fausses langues)
+		
+	
+		$i = 0;
+		$marque = "";
+		# boucle qui remplace une fausse langue par un espace
+		while ($i < $nb_fausses_langues) {
+			$langue = $tab_fausses_langues[$i];
+			while ($texte =~m /(.*) $langue([ ,;?!].*)/i) {
+				$texte = $1." ".$2;
+				print TRACE ("\n	<= $langue");
+			}
+			$i++;
+		}
+	
+		#...................................................................
+		# Comptage des langues par texte
+		$i = 0;
+		while ($i < $nb_langues) {
+			$langue = $tab_langues[$i];
+			if ($texte =~m / $langue[ ,;:?!~.!)-\/]+/i) {
+				$marque = $i;
+			}
+			if ($marque ne "") {
+				$marque = "";
+				
+				$langScore[$i][$num_periode]++; 
+				print TRACE ("$tab_langues[$i] : $langScore[$i]\n-------------------------------------");
+			}
+			$i++;
+		}
+
+	}
+
+	my $percent;
+	for $i ( 0 .. $#langScore ) {		
+   		for $j ( 0 .. $#{$langScore[$i]} ) {
+   			if ($j > 0) {
+   				$percent = sprintf "%.2f", scalar( $langScore[$i][$j] / $nbTextesParPeriode[$j]);
+	   			print RES ("$percent;")
+	   		}
+	   		else {
+   				print RES ("$langScore[$i][$j];");
+   			}
+    	}
+   	print RES ("\n");
+	}
+
+	close(TRACE);
+	close(RES);
+}
+
+
+
+#--------------------------
+# Fonction qui retourne un tableau de taille n+1(paramètre) 
+# initialisé à 0 avec nom de langue en premiere case
+sub initTab {
+	my $size = $_[0];
+	my $langue = $_[1];
+	my @tableau;
+
+	$tableau[0] = $langue;
+	for $i (1 .. $size) {
+		$tableau[$i] = 0;
+	}
+
+	return @tableau;
 }
